ea62c52c11f80e8217b005bb684b0f770d801c70
jakeyang <jakeyang10010@126.com>
2026-04-04 11:32:24 +0800

    之前的在本地写的代码

---
+++ src/main/java/io/github/yanjachi/superdrone/item/ModCreativeTab.java
@@ -0,0 +1,34 @@
+package io.github.yanjachi.superdrone.item;
+
+import io.github.yanjachi.superdrone.screen.SuperDrone;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeTab {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SuperDrone.MODID);
+
+    public static final RegistryObject<CreativeModeTab> SUPERDRONE_TAB = CREATIVE_MODE_TABS.register("superdrone_tab",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("creativetab.superdrone_tab"))
+                    .icon(() -> new ItemStack(ModItem.DRONE.get())) // 这个物品的图标会变成物品栏的图标
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItem.DRONE_CORE.get());
+                        output.accept(ModItem.DRONE.get());
+                        output.accept(ModItem.GEM_CHIP.get());
+                        output.accept(ModItem.BRASHLESS_MOTOR.get());
+                        // 后续新增物品继续往这里加：
+                        // output.accept(ModItems.XXX.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/io/github/yanjachi/superdrone/item/ModItem.java
@@ -0,0 +1,32 @@
+package io.github.yanjachi.superdrone.item;
+
+import io.github.yanjachi.superdrone.screen.SuperDrone;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItem {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SuperDrone.MODID);
+
+    // 示例物品：无人机核心
+    public static final RegistryObject<Item> DRONE_CORE = ITEMS.register("drone_core",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DRONE = ITEMS.register("drone",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GEM_CHIP = ITEMS.register("gem_chip",
+            () -> new Item(new Item.Properties().stacksTo(1).durability(300)));
+    public static final RegistryObject<Item> BRASHLESS_MOTOR = ITEMS.register("brashless_motor",
+            () -> new Item(new Item.Properties()));
+
+    // 你后续可以继续这样加：
+    // public static final RegistryObject<Item> BATTERY = ITEMS.register("battery",
+    //         () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
